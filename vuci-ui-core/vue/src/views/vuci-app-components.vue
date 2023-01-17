dfafd48ebcba11a296ea4ec84f4dc32fc0c8c778
mokupsas <mockus.ri@gmail.com>
2023-01-17 08:54:21 +0200

    added option to add new section

--- vuci-ui-core/vue/src/views/vuci-app-components.vue
+++ vuci-ui-core/vue/src/views/vuci-app-components.vue
@@ -30,12 +30,12 @@
 
     <div>
         <vuci-form uci-config="vuci_components_task">
-            <vuci-typed-section type="interface" :columns="columns">
+            <vuci-typed-section type="interface" :columns="columns" addremove :add="addItem">
                 <template #name="{ s }">
                     <vuci-form-item-dummy :uci-section="s"  name="name"/>
                 </template>
                 <template #address="{ s }">
-                    <vuci-form-item-dummy :uci-section="s"  name="address" rules="netmask4"/>
+                    <vuci-form-item-dummy :uci-section="s"  name="address" rules="ipaddr"/>
                 </template>
                 <template #netmask="{ s }">
                     <vuci-form-item-dummy :uci-section="s"  name="netmask" rules="netmask4"/>
@@ -45,8 +45,11 @@
                     <a-button type="danger" style="margin-right: 10px" size="small">{{ $t('Delete') }}</a-button>
                 </template>
             </vuci-typed-section>
-
-            <template slot="footer"><div></div></template>
+            <label class="field-label" for="enable">Interface name</label>
+            <div class="field-input">
+              <input type="text" class="input">
+            </div>
+            <!-- <template slot="footer"><div></div></template> -->
         </vuci-form>
 
         <!--
@@ -135,9 +138,37 @@ export default {
     },
     modalTitle (name) {
       return `Interface ${name}`
+    },
+    addItem (self) {
+      this.$prompt({
+        title: 'Add',
+        placeholder: 'Please input a name',
+        validator: value => {
+          if (self.sections.filter(s => s.name === value).length > 0) {
+            return 'The name already exist'
+          }
+        }
+      }).then(value => {
+        const sid = this.addSection('interface')
+        this.$uci.set('vuci_components_task', sid, 'name', value)
+        this.saveUci()
+      })
+    },
+    addSection (type, name) {
+      const sid = this.$uci.add('vuci_components_task', type, name)
+      return sid
+    },
+    saveUci () {
+      this.$uci.save().then(() => {
+        this.$uci.apply()
+      })
     }
+  },
+  created () {
+    // console.log(this.addSection('interface'))
   }
 }
 </script>
 
-<style></style>
+<style>
+</style>
