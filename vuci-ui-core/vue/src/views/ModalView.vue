825f313309bab629c7e6eda84a0b6fe3b39e4b02
mokupsas <mockus.ri@gmail.com>
2023-01-23 09:29:57 +0200

    removed ModalView unnecessery code

--- vuci-ui-core/vue/src/views/ModalView.vue
+++ vuci-ui-core/vue/src/views/ModalView.vue
@@ -2,7 +2,7 @@
     <a-modal v-model="show" :title="modalTitle(this.title)" @cancel="$emit('cancel')">
         <a-form-model>
             <vuci-form v-if="show" uci-config="vuci_components_task" @applied="$emit('applied')">
-                <vuci-named-section v-if="show" :name="this.uci" v-slot="{ s }" :add="inputValidate">
+                <vuci-named-section v-if="show" :name="this.uci" v-slot="{ s }">
                     <vuci-form-item-select :save="saveProto" initial="static" @change="optionChange" v-model="selectedProto" :uci-section="s" :options="protocols" :label="$t('Protocol')" name="proto"/>
                       <vuci-form-item-input :uci-section="s" :label="$t('Address')" name="address" depend="proto == 'static'" :rules="{ type: 'ip4addr' }" required/>
                       <vuci-form-item-input :uci-section="s" :label="$t('Netmask')" name="netmask" depend="proto == 'static'" :rules="{type: 'netmask4', min: 10}" required/>
@@ -51,11 +51,7 @@ export default {
       return `Interface ${name}`
     },
     optionChange (self) {
-      console.log('a1')
       this.selectedProto = self.model
-    },
-    inputValidate (self) {
-      console.log('a2')
     },
     validate (value) {
       if (value.length > 0) { return }
