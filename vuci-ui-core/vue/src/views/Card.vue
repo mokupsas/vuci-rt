2f0f225f0c932c78edfc99851765b880b2641025
mokupsas <mockus.ri@gmail.com>
2023-02-03 16:02:05 +0200

    updated Card with cpu load in head

--- vuci-ui-core/vue/src/views/Card.vue
+++ vuci-ui-core/vue/src/views/Card.vue
@@ -1,10 +1,15 @@
 <template>
-    <a-card class="card" :title="this.title">
-        <div class="row" v-for="(item, key) in data" :key="key">
-            <strong class="card-row-title">{{ item.title }}</strong>
-            <br>
-            <component :is="componentByType(item.type)" :percent="item.value" status="normal" size="small" class="card-span">{{ item.value }}</component>
+    <a-card class="card" :title="title">
+      <template slot="extra">
+        <div class="extra-wrapper">
+          <a-progress v-if="cpuLoad" class="extra-progress" style="width: 100px" :percent="2" size="small" :format="percent => `CPU LOAD ${percent}%`"></a-progress>
         </div>
+      </template>
+      <div class="row" v-for="(item, key) in data" :key="key">
+          <strong class="card-row-title">{{ item.title }}</strong>
+          <br>
+          <component :is="componentByType(item.type)" :percent="item.value" status="normal" size="small" class="card-span">{{ item.value }}</component>
+      </div>
 
       <slot></slot>
     </a-card>
@@ -15,7 +20,8 @@ export default {
   name: 'Card',
   props: {
     title: { required: true },
-    data: { required: true }
+    data: { required: true },
+    cpuLoad: { default: null }
   },
   methods: {
     componentByType (type) {
@@ -49,5 +55,22 @@ export default {
 .card-span {
   color: grey;
   font-size: 11px;
+}
+
+.extra-wrapper {
+  height: 36px;
+}
+
+.extra-progress {
+  float: right;
+  width: 50px;
+}
+
+.extra-progress .ant-progress-text {
+  display: inline;
+}
+
+.extra-progress .ant-progress-inner {
+  margin-left: 12px;
 }
 </style>
